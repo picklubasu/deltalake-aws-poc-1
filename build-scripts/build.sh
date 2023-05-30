e47e97a987207332f97fe31b7c62e818255d7643
Saptarsi Basu <basu.saptarsi91@gmail.com>
2023-05-30 18:15:54 +0530

    Uploading build scripts

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -22,7 +22,7 @@ package_application() {
 package_deploy() {
 	#sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.yml --stack-name llypharma-data-execution --parameter-overrides "$(jq -j 'to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' params.json)"
 	#SAM_PARAMETERS=$( cat ${CODEBUILD_SRC_DIR}/param.json | jq -r ' .Parameters | to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' )
-	sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.yml --stack-name deltalake-aws-poc-build-stack
+	sam deploy --template-file ${CODEBUILD_SRC_DIR}/${TEMPLATE_PATH} --stack-name deltalake-aws-poc-build-stack
 }
 
 echo "Starting build - $(date)"
--- build-scripts/pre_build.sh
+++ build-scripts/pre_build.sh
@@ -12,7 +12,7 @@ package_application() {
 package_deploy() {
 	#sam deploy --template-file ${CODEBUILD_SRC_DIR}/template.yml --stack-name llypharma-data-execution --parameter-overrides "$(jq -j 'to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' params.json)"
 	#SAM_PARAMETERS=$( cat ${CODEBUILD_SRC_DIR}/param.json | jq -r ' .Parameters | to_entries[] | "\(.key)='\\\"'\(.value)'\\\"''\ '"' )
-	sam deploy --template-file ${CODEBUILD_SRC_DIR}/prebuild_template.yml --stack-name 	deltalake-aws-poc-pre-build-stack
+	sam deploy --template-file ${CODEBUILD_SRC_DIR}/${PREBUILD_TEMPLATE_PATH} --stack-name deltalake-aws-poc-pre-build-stack
 }
 
 # Copy python packages
